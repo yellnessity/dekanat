05ac924dabe3dadbfcaa50c9e9534739f8d73a88
Egor Shchukin <diaz99@mail.ru>
2019-09-29 03:09:27 +0500

    Updates

---
+++ precache-manifest.c38ab1802393f9b30f6d0220c639618b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68d3f9c97c6b6fedd303d1e99690a86",
+    "url": "/dekanat/index.html"
+  },
+  {
+    "revision": "508112ae3e16baf0d4a5",
+    "url": "/dekanat/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "580bff79c144fa4b5ad0",
+    "url": "/dekanat/static/js/2.a0cd2830.chunk.js"
+  },
+  {
+    "revision": "508112ae3e16baf0d4a5",
+    "url": "/dekanat/static/js/main.05247925.chunk.js"
+  },
+  {
+    "revision": "6e055b967a8fddbb3f93",
+    "url": "/dekanat/static/js/runtime-main.997cd9ff.js"
+  }
+]);
